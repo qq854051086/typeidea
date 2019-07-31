fbf498199bbcb8a350ee35102538e1355d9e24e2
qq854051086 <854051086@qq.com>
2019-07-31 16:17:57 +0800

    finish 6.2
    完成了简单的后台配置，下一节将抽取save方法和get_queryset方法

--- typeidea/blog/admin.py
+++ typeidea/blog/admin.py
@@ -2,14 +2,40 @@ from django.contrib import admin
 from django.urls import reverse
 from django.utils.html import format_html
 
+from .adminforms import PostAdminForm
+from .models import Post, Category, Tag
+from typeidea.base_admin import BaseOwnerAdmin
+from typeidea.custom_site import custom_site
 
 # Register your models here.
-from .models import Post, Category, Tag
+
+
+
+class CategoryOwnerFilter(admin.SimpleListFilter):
+    '''自定义过滤器之展示当前用户分类'''
+    title = '分类过滤器'
+    parameter_name= 'owner_category'
+    def lookups(self, request, model_admin):
+        return Category.objects.filter(owner=request.user).values_list('id', 'name')
+
+    def queryset(self, request, queryset):
+        category_id = self.value()
+        if category_id:
+            return queryset.filter(category_id=self.value())
+        return queryset
+
+
+class PostInline(admin.TabularInline):   #可选择继承admin。StackedInline，以获取不同的展示样式
+    fields = ('title','desc')
+    extra = 1  #控制额外多几个
+    model = Post
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
+    inlines = [PostInline,]
     list_display = ('name', 'owner','status', 'is_nav', 'created_time', 'post_count')
     fields = ('name', 'status', 'is_nav', 'created_time', 'owner')
+
     #这里重写的save_model方法，即使用户选择其他用户，也会被写入为当前用户
 
     def save_model(self, request, obj, form, change):
@@ -31,14 +57,15 @@ class TagAdmin(admin.ModelAdmin):
         return super(TagAdmin, self).save_model(request, obj, form, change)
 
 
-@admin.register(Post)
+
+@admin.register(Post, site=custom_site)
 class PostAdmin(admin.ModelAdmin):
     list_display = ['title', 'category', 'status',
-                    'created_time', 'operator'
+                    'created_time', 'owner', 'operator'
     ]
     list_display_links = []  #点击那些字段可以进入编辑
 
-    list_filter = ['category',]
+    list_filter = [CategoryOwnerFilter]   #自定义右侧过滤内容为当前用户的内容
     search_fields = ['title', 'category__name']
 
 
@@ -48,19 +75,48 @@ class PostAdmin(admin.ModelAdmin):
     #编辑页面
     save_on_top = True    #保存、编辑、编辑并新建按钮是否在顶部展示
 
-    fields = (
-        ('category', 'title'),
-        'desc', 'status', 'content',
-        'tag',
-    )   #元组用于在同一行展示字段
+    exclude = ('owner',)   #指定哪些字段不展示
+    # fields = (
+    #     ('category', 'title'),
+    #     'desc', 'status', 'content',
+    #     'tag'
+    # )   #元组用于在同一行展示字段
+
+    fieldsets = (
+        ('基础配置',{
+            'description': '基础配置描述',
+            'fields':(
+                ('title', 'category'),
+                'status',
+            ),
+        }),
+        ('内容',{
+            'fields':(
+                'desc','content',
+            ),
+        }),
+        ('额外信息',{
+            'classes':('collapse',),
+            'fields':('tag',),
+        })
+    )
 
     def operator(self, obj):
         return format_html(
             '<a href="{}">编辑</a>',
-            reverse('admin:blog_post_change', args=(obj.id,))
+            reverse('cus_admin:blog_post_change', args=(obj.id,))
         )
     operator.short_description = '操作'    #自定义字段
+
+    # form = PostAdminForm    #自定义的form格式
 
     def save_model(self, request, obj, form, change):
         obj.owner = request.user
         return super(PostAdmin, self).save_model(request, obj, form, change)
+
+    def get_queryset(self, request):
+        qs = super(PostAdmin, self).get_queryset(request)
+        return qs.filter(owner=request.user)
+
+
+
